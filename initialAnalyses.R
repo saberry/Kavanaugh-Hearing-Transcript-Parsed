8efb295f6f2770b1d377651778cd25c414496971
Seth Berry <seth.berry@nd.edu>
2018-09-30 21:43:16 -0400

    some starting places

---
+++ initialAnalyses.R
@@ -0,0 +1,77 @@
+#######################
+### Initial Goofing ###
+#######################
+
+library(ggplot2)
+
+library(sentimentr)
+
+library(qdap)
+
+kavTrans = read.transcript("qdapTranscript.csv", 
+                           col.names = c("name", "commentFull"), 
+                           text.var = "commentFull", header = TRUE)
+
+allData = read.csv("parsedTranscript.csv", stringsAsFactors = FALSE)
+
+allData = allData %>% 
+  select(name, party) %>% 
+  distinct()
+
+kavTrans = left_join(kavTrans, allData, by = "name")
+
+kavTrans$party[is.na(kavTrans$party)] = "witness"
+
+## Person Formality
+
+personFormality = formality(kavTrans$commentFull, kavTrans$name, parallel = TRUE)
+
+plot(personFormality)
+
+## Wordclouds by person
+
+topTalkers = kavTrans %>% 
+  filter(name == "KAVANAUGH" | name == "FORD" |
+           name == "GRASSLEY" | name == "MITCHELL")
+
+stopWordList = unlist(c(tm::stopwords("SMART"), tm::stopwords("en"), "I"))
+
+trans_cloud(text.var = kavTrans$commentFull, grouping.var = kavTrans$name, 
+            stem = TRUE, stopwords = stopWordList, 
+            min.freq = 10, proportional = TRUE)
+
+## Conversation Flow
+
+gantt_plot(text.var = kavTrans$commentFull,
+           grouping.var = list(kavTrans$name, kavTrans$party), size = 4)
+
+## Sentence Splitting
+
+splitTrans = sentSplit(kavTrans, "commentFull")
+
+wordStat = word_stats(splitTrans$commentFull, splitTrans$name, tot = splitTrans$tot)
+
+plot(wordStat)
+
+## Sentiment
+
+load(url("https://github.com/saberry/courses/blob/master/hash_sentiment_vadar.RData?raw=true"))
+
+kavTrans = kavTrans %>% 
+  mutate(rowID = 1:nrow(.))
+
+allSentiment = sentimentr::sentiment(kavTrans$commentFull, hash_sentiment_vadar)
+
+allSentiment = kavTrans %>% 
+  select(name, party, rowID) %>% 
+  left_join(., allSentiment, by = c("rowID" = "element_id")) %>% 
+  mutate(sentenceOrder = 1:nrow(.))
+
+ggplot(allSentiment, aes(sentenceOrder, name, color = sentiment)) + 
+  geom_point(shape = "|", size = 5, alpha = .5) + 
+  scale_color_gradient2(low = "#a50026", mid = "#f7f7f7", high = "#313695") +
+  theme_dark()
+
+## Question Type
+
+questionTypes = question_type(splitTrans$commentFull, splitTrans$name, neg.cont = TRUE)
